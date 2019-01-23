6e4b8b56934bdcfe2b9290de87a3b883d69b05c4
knyg <kevin1992yang@gmail.com>
2019-01-23 04:56:32 -0800

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import NavBar from './Components/NavBar.jsx';
 
 class App extends Component {
   render() {
     return (
-      <div>Hello</div>
+      <div>
+        <NavBar></NavBar>
+      </div>
     );
   }
 }
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Navbar, Nav, NavItem, MenuItem, NavDropdown } from 'react-bootstrap';
+
+class NavBar extends React.Component {
+
+    render() {
+        return (
+            <Navbar collapseOnSelect staticTop>
+                <Navbar.Header style={{ marginLeft: '33%' }}>
+                    <Navbar.Brand>
+                        <a href="#brand">Avocado Restaurant</a>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header >
+                <Navbar.Collapse>
+                    <Nav>
+                        <NavDropdown eventKey={3} id="basic-nav-dropdown">
+                            <MenuItem eventKey={3.1}>Avocado Chef</MenuItem>
+                            <MenuItem eventKey={3.2}>Menu</MenuItem>
+                            <MenuItem eventKey={3.3}>Reservation</MenuItem>
+                            <MenuItem eventKey={3.3}>More About Avocados</MenuItem>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        );
+    };
+
+}
+
+export default NavBar;
