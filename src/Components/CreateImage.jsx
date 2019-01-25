bc529e541889d8461c288ff31b46c34d846c5e1f
knyg <kevin1992yang@gmail.com>
2019-01-25 06:52:17 -0800

    tidy up images

--- src/Components/CreateImage.jsx
+++ src/Components/CreateImage.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Image } from 'react-bootstrap';
 
-import avocado from '../avocado_images/avocado.jpg';
+import avocado from '../avocado_images/avocado_plant.jpg';
 
-const CreateImage = (props) => {
+const CreateImage = () => {
     return (
-        <Image responsive circle thumbnail src={avocado} ></Image>
+        <Image responsive circle thumbnail src={avocado} style={{ display: 'block', margin: '0 auto' }}></Image>
     );
 };
 
