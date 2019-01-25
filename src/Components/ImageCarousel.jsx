e7a4b4153882bd3615d33f71469dffc3d2ee9804
knyg <kevin1992yang@gmail.com>
2019-01-25 02:44:05 -0800

    rearrange and edit images

--- src/Components/ImageCarousel.jsx
+++ src/Components/ImageCarousel.jsx
@@ -3,37 +3,49 @@ import { Carousel } from 'react-bootstrap';
 
 //Images
 import avocado1 from '../avocado_images/avo1.jpg';
+import avocado2 from '../avocado_images/avo2.jpg';
+import avocado3 from '../avocado_images/avo3.jpg';
 import avocado4 from '../avocado_images/avo4.jpg';
 import avocado5 from '../avocado_images/avo5.jpg';
 import avocado6 from '../avocado_images/avo6.jpg';
 import avocado7 from '../avocado_images/avo7.jpg';
-import avocado9 from '../avocado_images/avo9.jpg';
-import avocado10 from '../avocado_images/avo10.jpg';
+import avocado8 from '../avocado_images/avo8.jpg';
 
 class ImageCarousel extends Component {
     render() {
         return (
-            <Carousel interval={2000} style={{ height: '100vh' }} >
-                <Carousel.Item active>
+            <Carousel pauseOnHover={false} slide={true} interval={2500} style={{ height: '100vh' }} >
+
+                <Carousel.Item>
                     <img style={{ width: '100%', height: '100vh' }} alt="900x500" src={avocado1} />
                 </Carousel.Item>
+
+                <Carousel.Item>
+                    <img style={{ width: '100%', height: '100vh' }} alt="900x500" src={avocado2} />
+                </Carousel.Item>
+
+                <Carousel.Item>
+                    <img style={{ width: '100%', height: '100vh' }} alt="900x500" src={avocado3} />
+                </Carousel.Item>
+
                 <Carousel.Item>
                     <img style={{ width: '100%', height: '100vh' }} alt="900x500" src={avocado4} />
                 </Carousel.Item>
+
                 <Carousel.Item>
                     <img style={{ width: '100%', height: '100vh' }} alt="900x500" src={avocado5} />
                 </Carousel.Item>
+
                 <Carousel.Item>
                     <img style={{ width: '100%', height: '100vh' }} alt="900x500" src={avocado6} />
                 </Carousel.Item>
+
                 <Carousel.Item>
                     <img style={{ width: '100%', height: '100vh' }} alt="900x500" src={avocado7} />
                 </Carousel.Item>
+
                 <Carousel.Item>
-                    <img style={{ width: '100%', height: '100vh' }} alt="900x500" src={avocado9} />
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img style={{ width: '100%', height: '100vh' }} alt="900x500" src={avocado10} />
+                    <img style={{ width: '100%', height: '100vh' }} alt="900x500" src={avocado8} />
                 </Carousel.Item>
             </Carousel>
         );
