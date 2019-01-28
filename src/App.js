d726d3cb59d50bc58a2f117686c1bf72c56d427d
knyg <kevin1992yang@gmail.com>
2019-01-28 06:59:04 -0800

    restructured elements into react-router-dom

--- src/App.js
+++ src/App.js
@@ -18,53 +18,52 @@ import Origin from './Components/Origin';
 class App extends Component {
   render() {
     return (
-      <div>
-        <NavBar />
+      <Router>
 
-        <JumboTron />
+        <div className='AppWrapper'>
 
-        <InbetweenText>
-          EXPLORE THE POSSIBILITIES
-        </InbetweenText>
+          <Route path='/' component={NavBar} />
+          <Route exact path='/' component={JumboTron} />
+          <Route exact path='/' render={(props) => <InbetweenText {...props} children={<span>EXPLORE THE POSSIBILITIES</span>} />} />
+          <Route exact path='/'
+            render={(props) => <TextArea
+              {...props}
+              children={
+                <p>"The avocado serves as a culinary chameleon — something that can go with a variety of dishes while disguised in many different ways. And including avocados as a main ingredient in sauces gives chefs the ability to customize a number of dishes with varying flavor profiles."
+                    <br />
+                  <span>-&nbsp;</span>
+                  <a href='https://avocadosfrommexico.com/foodservice/ask-foodservice/the-possibilities-are-endless-with-sauceabilities-2-0/' target='_blank' rel="noopener noreferrer">
+                    avocadosfrommexico.com
+                    </a>
+                </p>}//closes children
+            />//closes TextArea
+            }//closes Render
+          />{/*closes Route*/}
+          <Route exact path='/' component={ImageCarousel} />
+          <Route exact path='/' render={(props) => <InbetweenText {...props} children={<span>DISCOVER THE FLAVORS</span>} />} />
+          <Route exact path='/'
+            render={(props) => <TextArea {...props}
+              children={<p>"Avocado has a rich, buttery and creamy flavor. Depending on what cultivar it is, the avocado can have hints of sweetness as well as a mild, earthy vegetable taste. When it is fully ripe, the avocado can have a delicate, hearty flavor."
+            <br />
+                <span>-&nbsp;</span>
+                <a href='https://lovebackyard.com/what-does-avocado-taste-like/' target='_blank' rel="noopener noreferrer">
+                  lovebackyard.com
+                </a>
+              </p>}//closes children
+            />//closes TextArea
+            }//closes Render
+          />{/* closes Route */}
+          <Route exact path='/' render={(props) => <Button {...props} href='/recipes' children={<span>RECIPES</span>} />} />
+          <Route exact path='/' component={FlavorImages} />
+          <Route exact path='/' render={(props) => <InbetweenText {...props} children={<span>LEARN THEIR ORIGIN</span>} />} />
+          <Route exact path='/' render={(props) => <Button {...props} href='/origin' children={<span>ORIGIN</span>} />} />
+          <Route exact path='/' render={(props) => <CreateImage {...props} circle={true} thumbnail={true} src={require('./avocado_images/avocado_plant.jpg')} style={{ display: 'block', margin: '0 auto' }} />} />
 
-        <TextArea>
-          "The avocado serves as a culinary chameleon — something that can go with a variety of dishes while disguised in many different ways. And including avocados as a main ingredient in sauces gives chefs the ability to customize a number of dishes with varying flavor profiles."<br />-&nbsp;
-          <a href='https://avocadosfrommexico.com/foodservice/ask-foodservice/the-possibilities-are-endless-with-sauceabilities-2-0/' target='_blank' rel="noopener noreferrer">
-            avocadosfrommexico.com
-          </a>
-        </TextArea>
+        </div>
 
-        <ImageCarousel />
-
-        <InbetweenText>
-          DISCOVER THE FLAVORS
-        </InbetweenText>
-
-        <TextArea>
-          "Avocado has a rich, buttery and creamy flavor. Depending on what cultivar it is, the avocado can have hints of sweetness as well as a mild, earthy vegetable taste. When it is fully ripe, the avocado can have a delicate, hearty flavor."<br />-&nbsp;
-          <a href='https://lovebackyard.com/what-does-avocado-taste-like/' target='_blank' rel="noopener noreferrer">
-            lovebackyard.com
-          </a>
-        </TextArea>
-
-        <Button href='/recipes'>
-          Recipes
-        </Button>
-
-        <FlavorImages />
-
-        <InbetweenText>
-          LEARN THEIR ORIGIN
-        </InbetweenText>
-
-        <Button href='/origin'>
-          Origin
-        </Button>
-
-        <CreateImage circle='true' thumbnail='true' style={{ display: 'block', margin: '0 auto' }} src={require('./avocado_images/avocado_plant.jpg')} />
-      </div>
+      </Router >
     );
-  }
-}
+  };
+};
 
 export default App;
