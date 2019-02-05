6afa69971f961b4a22ac5f30225577146eea8a82
kyblockstacking <kevin1992yang@gmail.com>
2019-02-05 07:56:53 -0800

    restructure styles

--- src/Components/FlavorImages.jsx
+++ src/Components/FlavorImages.jsx
@@ -6,25 +6,25 @@ import CreateImage from './CreateImage';
 const FlavorImages = () => {
 
     const styles = {
-        CreateImage: {
+        createImage: {
             display: 'inline-block'
         }
-    }
+    };
 
     return (
         <div style={{ textAlign: 'center' }}>
 
-            <CreateImage thumbnail={true} style={styles.CreateImage} rounded src={require('../avocado_images/avocado_flavor1.jpg')}></CreateImage>
+            <CreateImage thumbnail={true} style={styles.createImage} rounded src={require('../avocado_images/avocado_flavor1.jpg')}></CreateImage>
 
-            <CreateImage thumbnail={true} style={styles.CreateImage} rounded src={require('../avocado_images/avocado_flavor2.jpg')}></CreateImage>
+            <CreateImage thumbnail={true} style={styles.createImage} rounded src={require('../avocado_images/avocado_flavor2.jpg')}></CreateImage>
 
-            <CreateImage thumbnail={true} style={styles.CreateImage} rounded src={require('../avocado_images/avocado_flavor3.jpg')}></CreateImage>
+            <CreateImage thumbnail={true} style={styles.createImage} rounded src={require('../avocado_images/avocado_flavor3.jpg')}></CreateImage>
 
-            <CreateImage thumbnail={true} style={styles.CreateImage} rounded src={require('../avocado_images/avocado_flavor4.jpg')}></CreateImage>
+            <CreateImage thumbnail={true} style={styles.createImage} rounded src={require('../avocado_images/avocado_flavor4.jpg')}></CreateImage>
 
-            <CreateImage thumbnail={true} style={styles.CreateImage} rounded src={require('../avocado_images/avocado_flavor5.jpg')}></CreateImage>
+            <CreateImage thumbnail={true} style={styles.createImage} rounded src={require('../avocado_images/avocado_flavor5.jpg')}></CreateImage>
 
-            <CreateImage thumbnail={true} style={styles.CreateImage} rounded src={require('../avocado_images/avocado_flavor6.jpg')}></CreateImage>
+            <CreateImage thumbnail={true} style={styles.createImage} rounded src={require('../avocado_images/avocado_flavor6.jpg')}></CreateImage>
 
         </div>
     );
