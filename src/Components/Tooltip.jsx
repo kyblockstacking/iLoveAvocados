c142d3778bc8203eee278bb0db2f63cfb89fe16f
kyblockstacking <kevin1992yang@gmail.com>
2019-02-05 08:19:58 -0800

    add classNames

--- src/Components/Tooltip.jsx
+++ src/Components/Tooltip.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 
 const LinkWithTooltip = ({ id, children, tooltip }) => {
+
     return (
         <OverlayTrigger
+            className='tooltip'
             overlay={<Tooltip id={id}>{tooltip}</Tooltip>}
             delayShow={300}
             delayHide={150}
             placement='right'
         >
-            <span>{children}</span>
+            <span className='tooltipText'>{children}</span>
         </OverlayTrigger>
     );
+
 };
 
 export default LinkWithTooltip;
