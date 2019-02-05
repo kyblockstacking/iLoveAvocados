949439c931d833e72b6ed183e09f9fab91f67897
kyblockstacking <kevin1992yang@gmail.com>
2019-02-05 07:53:54 -0800

    destructure props

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+
+//components
 import Tooltip from './Tooltip';
 
 class Button extends Component {
--- src/Components/FlavorImages.jsx
+++ src/Components/FlavorImages.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
 
+//components
 import CreateImage from './CreateImage';
 
 const FlavorImages = () => {
+
+    const styles = {
+        CreateImage: {
+            display: 'inline-block'
+        }
+    }
+
     return (
         <div style={{ textAlign: 'center' }}>
 
-            <CreateImage thumbnail={true} style={{ display: 'inline-block' }} rounded src={require('../avocado_images/avocado_flavor1.jpg')}></CreateImage>
+            <CreateImage thumbnail={true} style={styles.CreateImage} rounded src={require('../avocado_images/avocado_flavor1.jpg')}></CreateImage>
 
-            <CreateImage thumbnail={true} style={{ display: 'inline-block' }} rounded src={require('../avocado_images/avocado_flavor2.jpg')}></CreateImage>
+            <CreateImage thumbnail={true} style={styles.CreateImage} rounded src={require('../avocado_images/avocado_flavor2.jpg')}></CreateImage>
 
-            <CreateImage thumbnail={true} style={{ display: 'inline-block' }} rounded src={require('../avocado_images/avocado_flavor3.jpg')}></CreateImage>
+            <CreateImage thumbnail={true} style={styles.CreateImage} rounded src={require('../avocado_images/avocado_flavor3.jpg')}></CreateImage>
 
-            <CreateImage thumbnail={true} style={{ display: 'inline-block' }} rounded src={require('../avocado_images/avocado_flavor4.jpg')}></CreateImage>
+            <CreateImage thumbnail={true} style={styles.CreateImage} rounded src={require('../avocado_images/avocado_flavor4.jpg')}></CreateImage>
 
-            <CreateImage thumbnail={true} style={{ display: 'inline-block' }} rounded src={require('../avocado_images/avocado_flavor5.jpg')}></CreateImage>
+            <CreateImage thumbnail={true} style={styles.CreateImage} rounded src={require('../avocado_images/avocado_flavor5.jpg')}></CreateImage>
 
-            <CreateImage thumbnail={true} style={{ display: 'inline-block' }} rounded src={require('../avocado_images/avocado_flavor6.jpg')}></CreateImage>
+            <CreateImage thumbnail={true} style={styles.CreateImage} rounded src={require('../avocado_images/avocado_flavor6.jpg')}></CreateImage>
 
         </div>
     );
