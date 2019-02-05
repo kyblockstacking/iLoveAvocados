d74bef9c36b444dbc6ff84d41474f4a00fcbe3ec
kyblockstacking <kevin1992yang@gmail.com>
2019-02-05 08:15:23 -0800

    restructure styles and add classNames

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -5,16 +5,31 @@ import { Navbar } from 'react-bootstrap';
 import MenuModal from './MenuModal';
 
 const NavBar = () => {
+
+    const styles = {
+        navbarHeader: {
+            display: 'inline-block',
+            textAlign: 'center',
+            width: '100%',
+            paddingTop: '10px'
+        },
+        navbarBrand: {
+            fontFamily: 'Poppins',
+            color: 'olive',
+            textDecoration: 'none',
+            margin: '0',
+            fontSize: '1.5em'
+        }
+    };
+
     return (
-        <Navbar fixedTop>
-            <Navbar.Header style={{ display: 'inline-block', textAlign: 'center', width: '100%', paddingTop: '10px' }}>
-                <div href='/' style={{ fontFamily: 'Poppins' }}>
-                    <a href='/' style={{ color: 'olive', textDecoration: 'none', margin: '0', fontSize: '1.5em' }}>
-                        Avocado.California
-                            </a>
-                    &nbsp;
-                        <MenuModal />
-                </div>
+        <Navbar className='navbar' fixedTop>
+            <Navbar.Header className='navbarHeader' style={styles.navbarHeader}>
+                <a className='navbarBrand' href='/' style={styles.navbarBrand}>
+                    Avocado.California
+                </a>
+                &nbsp;
+                <MenuModal />
             </Navbar.Header >
         </Navbar>
 
