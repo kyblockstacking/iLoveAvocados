bf9eb10339256410f53b4961acb160cacce6d8b5
kyblockstacking <kevin1992yang@gmail.com>
2019-02-05 08:18:18 -0800

    restructure props, styles, and add classNames

--- src/Components/TextArea.jsx
+++ src/Components/TextArea.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 
 const TextArea = (props) => {
+
+    const { children } = props;
+
+    const styles = {
+        textArea: {
+            color: '#4c6e66',
+            fontFamily: 'Archivo',
+            textAlign: 'center',
+            width: '40%',
+            margin: '0 auto 1em'
+        }
+    };
+
     return (
-        <div style={{ color: '#4c6e66', fontFamily: 'Archivo', textAlign: 'center', width: '40%', margin: '0 auto 1em' }}>
-            {props.children}
-        </div>
+        <div style={styles.textArea}>
+            {children}
+        </div >
     );
+
 };
 
 export default TextArea;
