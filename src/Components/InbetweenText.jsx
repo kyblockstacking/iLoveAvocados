810456f6c9c49976ae35fb2de21d27a956ade5cd
kyblockstacking <kevin1992yang@gmail.com>
2019-02-05 07:58:46 -0800

    deconstruct props and restructure styles

--- src/Components/InbetweenText.jsx
+++ src/Components/InbetweenText.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 
 const InbetweenText = (props) => {
+
+    const { children } = props;
+
+    const styles = {
+        inbetweenText: {
+            fontFamily: 'Archivo',
+            textAlign: 'center',
+            background: 'white',
+            padding: '1em 1em 0.5em 1em',
+            color: '#568203',
+            textShadow: '-1px -1px 2px #424242, 1px -1px 2px #424242, -1px 1px 2px #424242, 1px 1px 2px #424242',
+            fontSize: '2em'
+        }
+    };
+
     return (
-        <div style={{ fontFamily: 'Archivo', textAlign: 'center', background: 'white', padding: '1em 1em 0.5em 1em', color: '#568203', textShadow: '-1px -1px 2px #424242, 1px -1px 2px #424242, -1px 1px 2px #424242, 1px 1px 2px #424242', fontSize: '2em' }}>
-            {props.children}
+        <div style={styles.inbetweenText}>
+            {children}
         </div>
     );
 };
