9fd2444d140a40a566fb4397beecbd9225cbae9e
knyg <kevin1992yang@gmail.com>
2019-02-05 23:29:11 -0800

    add button UX/UI

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -8,7 +8,21 @@ class Button extends Component {
         super(props);
 
         this.state = {
-            backgroundColor: 'white',
+            buttonStyle: {
+                fontFamily: 'Archivo',
+                display: 'block',
+                margin: '0 auto',
+                textAlign: 'center',
+                border: '2px solid #568203',
+                padding: '0.5em',
+                marginBottom: '2vh',
+                borderRadius: '5px',
+                width: '200px',
+                textDecoration: 'none',
+                backgroundColor: 'white',
+                transition: 'all 0.3s ease 0s',
+                fontWeight: '900'
+            }
         };
     };
 
@@ -27,17 +41,38 @@ class Button extends Component {
                 marginBottom: '2vh',
                 borderRadius: '5px',
                 width: '200px',
-                backgroundColor: this.state.backgroundColor,
+                textDecoration: 'none',
+                backgroundColor: 'white',
+                transition: 'all 0.3s ease 0s',
+                fontWeight: '900'
             },
+            buttonHover: {
+                fontFamily: 'Archivo',
+                display: 'block',
+                margin: '0 auto',
+                textAlign: 'center',
+                border: '2px solid #568203',
+                padding: '0.5em',
+                marginBottom: '2vh',
+                borderRadius: '5px',
+                width: '200px',
+                textDecoration: 'none',
+                boxShadow: '0px 10px 20px #4c6e66',
+                color: '#fff',
+                transform: 'translateY(-5px)',
+                backgroundColor: '#4c6e66',
+                transition: 'all 0.3s ease 0s',
+                fontWeight: '900'
+            }
         };
 
         return (
             <a
                 className='button'
                 href={href}
-                onMouseEnter={() => { this.setState({ backgroundColor: '#4c6e66' }) }}
-                onMouseLeave={() => { this.setState({ backgroundColor: 'white' }) }}
-                style={styles.button}>
+                onMouseEnter={() => { this.setState({ buttonStyle: styles.buttonHover }) }}
+                onMouseLeave={() => { this.setState({ buttonStyle: styles.button }) }}
+                style={this.state.buttonStyle}>
                 <Tooltip
                     className='tooltip'
                     tooltip={tooltip}>
